bd8240ed904e3a26366db41474e2dd53b1da7534
Guillaume Lorente <guillaumelorente@orange.fr>
2025-10-03 01:38:59 +0200

    Maj seo.ts

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
 name: 'Guillaume Lorente',
 title: 'Guillaume Lorente — CV & Portfolio',
 description: 'Développeur full‑stack',
-url: 'https://votre-domaine.dev',
+url: 'https://mon-cv-umber-seven.vercel.app',
 links: {
 github: 'https://github.com/Guillaume-Lorente',
 linkedin: 'https://www.linkedin.com/in/guillaume-lorente-8a6b9a21b/',
