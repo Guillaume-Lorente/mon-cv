2a04b8b5a05025213fe7b9f676cc5f80d0d46798
Guillaume Lorente <guillaumelorente@orange.fr>
2026-01-08 16:52:09 +0100

    Update course page

--- src/app/parcours/page.tsx
+++ src/app/parcours/page.tsx
@@ -7,6 +7,8 @@ const items: TimelineItem[] = [
 { date: '2023', title: 'Responsable de bar', org: 'Globe Trotter Valfréjus', description: 'Accueil & service client, Gestion du bar, Travail en équipe, Adaptation, Rapidité d\'exécution' },
 { date: '2020 – 2022', title: 'Assistant communication digitale', org: 'Rémi Perrier Organisation Grenoble - Festival Musilac', description: 'Gestion de projets de communication, Rédaction & diffusion de contenus, SEA' },
 { date: '2020 – 2022', title: 'MSc 2 Marketing Digital & E-business - Manager de la Communication et du Marketing Digital', org: 'INSEEC Groupe OMNES Education Chambéry', description: 'Stratégies digitales & E-business, Community management, Analyse de performance, Création & pilotage de contenu' },
+{ date: '2019 – 2020', title: 'Bachelor Responsable International en Marketing Développement, Spécialité : Marketing Digital', org: 'INSEEC Groupe OMNES Education Chambéry', description: 'Communication interne/externe, Comportement de l\'omniconsommateur, Expérience utilisateur (UX/UI), Plan de communication, Stratégie et communication digitale' },
+{ date: '2015 – 2018', title: 'DUT Gestion Administrative et Commerciale des Organisations option *Musique', org: 'Université Savoie Mont Blanc', description: 'Gestion Commerciale et négociation, E-commerce & E-Marketing, Management des activités artistiques, Droit des relations professionnelles, Stratégie des organisations' },
 ];
 
 
