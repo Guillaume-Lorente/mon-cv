5065f46e87584bf6919e1b7e792e2cd6cd36886f
Guillaume Lorente <guillaumelorente@orange.fr>
2026-07-10 14:54:57 +0200

    Update HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ const featured = [
   { title: 'Villas Grande Anse', description: 'Site web de location de villas en Guadeloupe.', tags: ['Next.js', 'Express.js', 'Tailwind', 'PostgreSQL', 'SEO'], href: 'https://www.villas-grande-anse.com/', imageSrc: '/images/villas-grande-anse.png', imageAlt: 'Accueil du site web Villas Grande Anse' },
   { title: 'Moniteur de ski Tignes / Val d\'Isère', description: 'Site web de promotion pour moniteur de ski.', tags: ['Next.js', 'Tailwind', 'SEO', 'i18n'], href: 'https://alexandre-lorente.vercel.app/fr', imageSrc: '/images/alex.png', imageAlt: 'Accueil du site web Alexandre Lorente' },
   { title: 'Wedding & Event Planner', description: 'Site web d\'organisation d\'événements.', tags: ['Next.js', 'Tailwind', 'SEO'], href: 'https://www.evenementiels-en-lumieres.fr/', imageSrc: '/images/evenementiels.png', imageAlt: 'Accueil du site web Événementiels en Lumieres' },
-  { title: 'Nation Sounds Festival', description: 'Site web mobile-first d\'un festival de musique.', tags: ['React', 'Node.js', 'PostgreSQL', 'i18n'], href: 'https://nation-sounds.alwaysdata.net/', imageSrc: '/images/nation-sounds.png', imageAlt: 'Accueil du site web Nation Sounds Festival' },
 ];
 
 export default function HomePage() {
